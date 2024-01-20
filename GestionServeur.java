a237d7350e1a8af553806f0d3455f13128f55d14
Ferdoel Titouan <titouan.ferdoel@etu.univ-orleans.fr>
2024-01-20 16:29:48 +0100

    corrections faites

--- GestionServeur.java
+++ GestionServeur.java
@@ -44,15 +44,15 @@ class GestionServeur extends Thread {
                     try {
 
                         String reponse = (String) inputStream.readObject();
-                        if ("O".equals(reponse)) {
+                        if ("O".equals(reponse.toUpperCase())) {
                             // Renvoyer des informations au client (dans cet exemple, l'adresse IP)
                             this.serveur.addUser(this.nom);
                             outputStream.writeObject(
                                     "Votre compte a été créé avec succes, bienvenue: " + this.nom + "\n");
                             idConnection = 2;
                         } else {
-                            System.out.println(
-                                    "Au revoir ! Info -> : " + clientSocket.getInetAddress().getHostAddress());
+                            outputStream.writeObject(
+                                    "Au revoir\n");
                             break;
                         }
 
@@ -80,28 +80,28 @@ class GestionServeur extends Thread {
                             System.out.println("Supprime le message à l'ID : " + msg);
 
                             this.serveur.delete(msg, this.nom);
-                            outputStream.writeObject("Message de " +  this.nom+ " à l'ID " + msg + " supprimer \n");
+                            outputStream.writeObject("Message de " + this.nom + " à l'ID " + msg + " supprimer \n");
                         }
 
                         else if (clientCommand.startsWith("/refresh")) {
-                            try{
+                            try {
                                 try {
                                     String nbS = clientCommand.split(" ")[1];
                                     Integer nb = Integer.parseInt(nbS);
-                                    String msgs = this.serveur.refresh(this.nom,nb);
+                                    String msgs = this.serveur.refresh(this.nom, nb);
                                     System.out.println("REFRESHED par " + this.nom);
-                                    outputStream.writeObject("Les 10 derniers messages sont : \n \n" + msgs + "\n" );
+                                    outputStream.writeObject("Les 10 derniers messages sont : \n \n" + msgs + "\n");
                                 } catch (Exception e) {
-                                    System.out.println("Default 10");}
-                                    Integer nb = 10;
-                                    String msgs = this.serveur.refresh(this.nom,nb);
-                                    System.out.println("REFRESHED par " + this.nom);
-                                    outputStream.writeObject("Les 10 derniers messages sont : \n \n" + msgs + "\n" );
-                                
-                            }
-                            catch(Exception e){
+                                    System.out.println("Default 10");
+                                }
+                                Integer nb = 10;
+                                String msgs = this.serveur.refresh(this.nom, nb);
+                                System.out.println("REFRESHED par " + this.nom);
+                                outputStream.writeObject("Les 10 derniers messages sont : \n \n" + msgs + "\n");
+
+                            } catch (Exception e) {
                                 System.out.println("ERR");
-                            }                           
+                            }
 
                         }
 
@@ -109,7 +109,8 @@ class GestionServeur extends Thread {
                             System.out.println(
 
                                     "Client deconnecté ! Info -> : " + clientSocket.getInetAddress().getHostAddress());
-                            outputStream.writeObject("Au revoir\n");
+                            outputStream.writeObject(
+                                    "Au revoir\n");
                             break;
                         }
 
@@ -120,8 +121,7 @@ class GestionServeur extends Thread {
                             this.serveur.addMessage(this.nom, msg);
                             outputStream.writeObject("Message Envoyé -> " + msg + "\n");
 
-                        }
-                        else if ("/info".equals(clientCommand)) {
+                        } else if ("/info".equals(clientCommand)) {
                             // Renvoyer des informations au client (dans cet exemple, l'adresse IP)
                             String clientAddress = clientSocket.getInetAddress().getHostAddress();
                             System.out.println(this.serveur.loadJSON());
@@ -130,7 +130,14 @@ class GestionServeur extends Thread {
 
                         } else if (clientCommand.startsWith("/follow")) {
                             String user = clientCommand.split(" ")[1];
-                            if (this.serveur.getUsers().containsKey(user)) {
+                            if (user.equals(this.nom)) {
+                                outputStream.writeObject(
+                                        "Vous ne pouvez pas vous abonner à vous-même\n");
+
+                            } else if (this.serveur.getUsers().get(this.nom).contains(user)) {
+                                outputStream.writeObject(
+                                        "Vous êtes déjà abonné à " + user + "\n");
+                            } else if (this.serveur.getUsers().containsKey(user)) {
                                 this.serveur.addFollow(this.nom, user);
                                 outputStream.writeObject(
                                         "Vous êtes maintenant abonné à " + user + "\n");
