2f47859865e1729a948589bd9d3b2e259ccd3389
FOURNIER Cyprien <122486705+IRPYC37@users.noreply.github.com>
2024-01-20 17:34:42 +0100

    comm

--- Client.java
+++ Client.java
@@ -2,15 +2,26 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-
 import java.util.Scanner;
 
+/**
+ * La classe Client représente un client dans une application de messagerie.
+ * Elle gère la connexion au serveur, l'envoi et la réception de messages.
+ */
 public class Client {
-    private Socket socket;
-    private ObjectOutputStream outputStream;
-    private ObjectInputStream inputStream;
-    private String nom;
+    private Socket socket; // Le socket pour la connexion au serveur
+    private ObjectOutputStream outputStream; // Le flux de sortie pour envoyer des objets au serveur
+    private ObjectInputStream inputStream; // Le flux d'entrée pour recevoir des objets du serveur
+    private String nom; // Le nom du client
 
+    /**
+     * Constructeur de la classe Client.
+     * Il initialise la connexion au serveur et les flux d'entrée/sortie.
+     *
+     * @param serverAddress L'adresse du serveur à laquelle se connecter
+     * @param serverPort Le port du serveur sur lequel se connecter
+     * @param nom Le nom du client
+     */
     public Client(String serverAddress, int serverPort, String nom) {
         try {
             socket = new Socket(serverAddress, serverPort);
@@ -25,29 +36,25 @@ public class Client {
         }
     }
 
+    /**
+     * Cette méthode démarre le client.
+     * Elle lit les entrées de l'utilisateur et les envoie au serveur jusqu'à ce que l'utilisateur tape "/exit".
+     */
     public void startClient() {
-
         try {
-
             Scanner scanner = new Scanner(System.in);
-
             while (true) {
-                // Lit la console
                 String userInput = scanner.nextLine();
-
                 if ("/exit".equals(userInput)) {
                     outputStream.writeObject(userInput);
                     System.out.println("Au revoir.");
                     break;
                 }
-                // Envoie la commande au serveur
                 outputStream.writeObject(userInput);
                 String serverResponse;
                 try {
                     serverResponse = (String) inputStream.readObject();
-
                     System.out.println("Server : " + serverResponse);
-
                     if (serverResponse.equals("Au revoir\n")) {
                         break;
                     }
@@ -55,7 +62,6 @@ public class Client {
                     e.printStackTrace();
                     System.out.println("Erreur d'InputStream !");
                 }
-
             }
             scanner.close();
             outputStream.close();
@@ -66,17 +72,20 @@ public class Client {
         }
     }
 
+    /**
+     * Le point d'entrée de l'application.
+     * Il crée un nouveau client et démarre le client.
+     *
+     * @param args Les arguments de la ligne de commande
+     */
     public static void main(String[] args) {
         System.out.println(
                 "Bienvenue sur notre application de messagerie, veuillez renseigner le port du serveur ainsi que votre nom \n");
         Scanner scanner = new Scanner(System.in);
-
         String userInput = scanner.nextLine();
         String[] a = userInput.split(" ");
-
         Client client = new Client("localhost", Integer.parseInt(a[0]), a[1]);
         client.startClient();
-
         scanner.close();
     }
 }
