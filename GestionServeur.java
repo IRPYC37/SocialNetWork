858320b38ad4d6f7d6b283843d22f74fbc1c9337
Ferdoel Titouan <titouan.ferdoel@etu.univ-orleans.fr>
2024-01-20 14:23:23 +0100

    gerer users fait

--- GestionServeur.java
+++ GestionServeur.java
@@ -4,10 +4,14 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 class GestionServeur extends Thread {
+    private Serveur serveur;
     private Socket clientSocket;
+    private Integer idConnection = 0;
+    private String nom;
 
-    public GestionServeur(Socket socket) {
+    public GestionServeur(Socket socket, Serveur s) {
         this.clientSocket = socket;
+        this.serveur = s;
     }
 
     @Override
@@ -15,35 +19,100 @@ class GestionServeur extends Thread {
         try {
             ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
             ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
-
             while (true) {
+
                 // Lire la commande du client
                 String clientCommand;
-                try {
-                    clientCommand = (String) inputStream.readObject();
-                    // Traitement de la commande spécifique
-                    if ("/info".equals(clientCommand)) {
-                        // Renvoyer des informations au client (dans cet exemple, l'adresse IP)
-                        String clientAddress = clientSocket.getInetAddress().getHostAddress();
-                        outputStream.writeObject("Votre adresse IP est : " + clientAddress + "\n");
-                    }
 
-                    if ("/exit".equals(clientCommand)) {
-                        System.out.println("Client deconnecté ! Info -> : " + clientSocket.getInetAddress().getHostAddress());
-                        break;
-                        
+                if (idConnection == 0) {
+                    try {
+
+                        clientCommand = (String) inputStream.readObject();
+                        this.nom = clientCommand;
+                        if (this.serveur.getUsers().containsKey(this.nom)) {
+                            outputStream.writeObject("Bienvenue " + this.nom + "\n");
+                            idConnection = 2;
+                        } else {
+                            outputStream
+                                    .writeObject("Vous devez créer un compte. Créer votre compte? O/N \n");
+                            idConnection = 1;
+                        }
+                    } catch (Exception e) {
                     }
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                    ;
+                } else if (idConnection == 1) {
+                    try {
+
+                        String reponse = (String) inputStream.readObject();
+                        if ("O".equals(reponse)) {
+                            // Renvoyer des informations au client (dans cet exemple, l'adresse IP)
+                            this.serveur.addUser(this.nom);
+                            outputStream.writeObject(
+                                    "Votre compte a été créé avec succes, bienvenue: " + this.nom + "\n");
+                            idConnection = 2;
+                        } else {
+                            System.out.println(
+                                    "Au revoir ! Info -> : " + clientSocket.getInetAddress().getHostAddress());
+                            break;
+                        }
+
+                    } catch (Exception e) {
+                    }
+                } else {
+                    try {
+                        clientCommand = (String) inputStream.readObject();
+                        // Traitement de la commande spécifique
+                        if ("/info".equals(clientCommand)) {
+                            // Renvoyer des informations au client (dans cet exemple, l'adresse IP)
+                            String clientAddress = clientSocket.getInetAddress().getHostAddress();
+
+                            outputStream.writeObject(
+                                    "Votre adresse IP est : " + clientAddress + "\n" + this.serveur.getUsers());
+
+                        } else if (clientCommand.startsWith("/follow")) {
+                            String user = clientCommand.split(" ")[1];
+                            if (this.serveur.getUsers().containsKey(user)) {
+                                this.serveur.addFollow(this.nom, user);
+                                outputStream.writeObject(
+                                        "Vous êtes maintenant abonné à " + user + "\n");
+                            } else {
+                                outputStream.writeObject(
+                                        "Cet utilisateur n'existe pas\n");
+                            }
+
+                        }
+
+                        else if (clientCommand.startsWith("/unfollow")) {
+                            String user = clientCommand.split(" ")[1];
+                            if (this.serveur.getUsers().containsKey(user)) {
+                                this.serveur.deleteFollow(this.nom, user);
+                                outputStream.writeObject(
+                                        "Vous êtes maintenant désabonné de " + user + "\n");
+                            } else {
+                                outputStream.writeObject(
+                                        "Cet utilisateur n'existe pas\n");
+                            }
+
+                        }
+
+                        else if ("/exit".equals(clientCommand)) {
+                            System.out.println(
+                                    "Client deconnecté ! Info -> : " + clientSocket.getInetAddress().getHostAddress());
+                            break;
+
+                        }
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
 
-            } 
+            }
+
             outputStream.close();
-            inputStream.close(); 
+            inputStream.close();
             clientSocket.close();
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
